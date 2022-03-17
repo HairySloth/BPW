49ba7cbffaab71fb8de953e14cb1c8346d49b923
HairySloth <aidenwkok@gmail.com>
2022-03-17 17:36:19 +0100

    now on URP, finalized pick up script

--- BPW 2/Assets/Scripts/PickUp.cs
+++ BPW 2/Assets/Scripts/PickUp.cs
@@ -6,8 +6,10 @@ public class PickUp : MonoBehaviour
 {
     private GameObject heldObj;
     public float pickUpRange = 5;
+    public float pickUpRangeLarge = 50;
     public Transform holdParent;
     public float moveForce = 150;
+    public bool superPickUp = false;
 
 
     // Update is called once per frame
@@ -34,10 +36,46 @@ public class PickUp : MonoBehaviour
             }
         }
 
+        if (superPickUp == true)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (heldObj == null)
+                {
+                    LayerMask mask = LayerMask.GetMask("PickUp");
+
+                    RaycastHit hit;
+                    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickUpRangeLarge, mask))
+                    {
+                        PickUpObject(hit.transform.gameObject);
+                    }
+                }
+                else
+                {
+                    DropObject();
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if (superPickUp == false)
+            {
+                superPickUp = true; 
+            }
+            else
+            {
+                superPickUp = false;
+            }
+        }
+
+
+
         if (heldObj != null)
         {
             MoveObject();
         }
+    
     }
 
     void MoveObject()
@@ -57,6 +95,8 @@ public class PickUp : MonoBehaviour
             Rigidbody objRig = pickObj.GetComponent<Rigidbody>();
             objRig.useGravity = false;
             objRig.drag = 10;
+            objRig.angularDrag = 1;
+            objRig.mass = objRig.mass * 5;
 
             objRig.transform.parent = holdParent;
             heldObj = pickObj;
@@ -69,6 +109,8 @@ public class PickUp : MonoBehaviour
         Rigidbody heldRig = heldObj.GetComponent<Rigidbody>();
         heldRig.useGravity = true;
         heldRig.drag = 1;
+        heldRig.angularDrag = 0.05f;
+        heldRig.mass = heldRig.mass / 5;
 
         heldObj.transform.parent = null;
         heldObj = null;
